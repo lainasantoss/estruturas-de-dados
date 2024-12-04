c4c1ae0190219a2674d0a7e61de189ebaad4600c
Laina <179530115+lainasantoss@users.noreply.github.com>
2024-12-03 23:59:17 -0300

    Delete ListasDinamicas/src/main.kt

--- ListasDinamicas/src/main.kt
+++
@@ -1,7 +0,0 @@
-fun main (){
-    var pilha: Empilhavel = PilhaDinamica(8)
-    pilha.empilhar("Instituto")
-    pilha.empilhar("Federal")
-    println(pilha.imprimir()) // Vai imprimir o conteúdo da pilha
-    println(pilha.espiar())
-}
