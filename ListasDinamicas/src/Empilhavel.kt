df608e5e8b0654be697d6b36b43b2904474ce16a
Laina <179530115+lainasantoss@users.noreply.github.com>
2024-12-04 00:00:40 -0300

    Delete ListasDinamicas/src directory

--- ListasDinamicas/src/Empilhavel.kt
+++
@@ -1,9 +0,0 @@
-interface Empilhavel {
-    fun empilhar(dado: Any?)
-    fun desempilhar(): Any?
-    fun espiar(): Any?
-    fun atualizar(dado: Any?)
-    fun estaCheia(): Boolean
-    fun estaVazia(): Boolean
-    fun imprimir(): String
-}
--- ListasDinamicas/src/NoDuplo.kt
+++
@@ -1,4 +0,0 @@
-data class NoDuplo(var dado: Any? = null) {
-    var anterior: NoDuplo? = null
-    var proximo: NoDuplo? = null
-}
