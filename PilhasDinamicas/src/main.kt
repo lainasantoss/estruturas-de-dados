d7a71b390043808b91544073203e5ef2a5434db6
Laina <179530115+lainasantoss@users.noreply.github.com>
2024-12-03 23:57:37 -0300

    Pilhas Dinâmicas
    Pilhas Dinâmicas

---
+++ PilhasDinamicas/src/main.kt
@@ -0,0 +1,7 @@
+fun main (){
+    var pilha: Empilhavel = PilhaDinamica(8)
+    pilha.empilhar("Instituto")
+    pilha.empilhar("Federal")
+    println(pilha.imprimir()) // Vai imprimir o conteúdo da pilha
+    println(pilha.espiar())
+}
